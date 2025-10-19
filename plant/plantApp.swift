6c4973e7e71727ef32eded78ffec38ae53d59df6
wasan <twasanj@gmail.com>
2025-10-19 11:33:09 +0300

    Initial Commit

---
+++ plant/plantApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantApp.swift
+//  plant
+//
+//  Created by wasan jayid althagafi on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
