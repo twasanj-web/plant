c8b769ef4beeec4d2cdffa706b6df8eb1f1e30e6
wasan <twasanj@gmail.com>
2025-10-22 20:40:37 +0300

    First version before MVVM update

---
+++ plant/SetReminder.swift
@@ -0,0 +1,18 @@
+//
+//  SetReminder.swift
+//  plant
+//
+//  Created by wasan jayid althagafi on 28/04/1447 AH.
+//
+
+import SwiftUI
+
+struct SetReminder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetReminder()
+}
---
+++ plant/fp.swift
@@ -0,0 +1,18 @@
+//
+//  fp.swift
+//  plant
+//
+//  Created by wasan jayid althagafi on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+struct fp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    fp()
+}
